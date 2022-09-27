253aeddd0e13e149fbaecb0f3d2176f6c01b53a5
Pedro <pjxp44@gmail.com>
2022-09-27 15:39:45 +0200

    hovers done in picture, h2 and h3

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import img1 from "/images/image-equilibrium.jpg";
 import nftIcon from "/images/icon-ethereum.svg";
 import clockIcon from "/images/icon-clock.svg";
+import viewIcon from "/images/icon-view.svg";
 import avatar from "/images/image-avatar.png";
 
 export default function Home() {
@@ -33,18 +34,44 @@ export default function Home() {
       <main className="bg-veryDarkBlueMain | text-softBlue | font-body | h-screen | p-7 | flex | flex-col | justify-center | items-center">
         <article className="bg-veryDarkBlueCard | p-6 | rounded-xl | flex | flex-col | gap-4 | max-w-xs">
           <header className="flex | flex-col | gap-4 ">
-            <Image
-              src={img1}
-              alt="picture that shows the representation of the NFT"
-              layout="responsive"
-              className="rounded-lg"
-            />
-            <h2 className=" text-white | text-xl | font-semibold">
-              Equilibrium #3429
+            <picture
+              className="relative"
+              onMouseOver={() => {
+                document.getElementById("hover-img").classList.toggle("hidden");
+              }}
+              onMouseOut={() => {
+                document.getElementById("hover-img").classList.toggle("hidden");
+              }}
+            >
+              <Image
+                id="main-image"
+                src={img1}
+                alt="picture that shows the representation of the NFT"
+                layout="responsive"
+                className="rounded-lg"
+              />
+              <div
+                id="hover-img"
+                className="bg-cyan | absolute | w-full | h-full | top-0 | bg-opacity-40 | rounded-lg | hidden"
+              >
+                <picture className="flex | justify-center | items-center | h-full || hover:cursor-pointer">
+                  <Image
+                    src={viewIcon}
+                    alt="Icon of an eye"
+                    layout="fixed"
+                    width={30}
+                    height={30}
+                  />
+                </picture>
+              </div>
+            </picture>
+            <h2 className=" flex | text-white | text-xl | font-semibold">
+              <a className="hover:text-cyan | hover:cursor-pointer">
+                Equilibrium #3429
+              </a>
             </h2>
           </header>
           <p className="text-md | font-light">
-            {" "}
             Our Equilibrium collection promotes balance and calm.
           </p>
           <section className="flex | flex-row | justify-between | items-center">
@@ -59,13 +86,14 @@ export default function Home() {
           <hr className="text-veryDarkBlueLine" />
           <footer className="flex | items-center | gap-5 ">
             <picture className="relative | w-8 | h-8 | border-[1px] | border-solid | border-white | rounded-full">
-              <Image
-                src={avatar}
-                alt="Creator's avatar"
-                layout="fill"
-              />
+              <Image src={avatar} alt="Creator's avatar" layout="fill" />
             </picture>
-            <h3 className="font-light">Creation of <strong className="text-white">Jules Wyvern</strong></h3>
+            <h3 className="font-light">
+              Creation of{" "}
+              <a className="text-white || hover:text-cyan | hover:cursor-pointer">
+                Jules Wyvern
+              </a>
+            </h3>
           </footer>
         </article>
       </main>
